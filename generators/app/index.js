bc41bf5fa7766272647486a799b5f232b5d4493d
Craig Simko <craigs1701@gmail.com>
2018-04-12 21:21:21 -0400

    "yo casapps" now scaffolds a generic TypeScript project.
    The project is set up with VSCode launch config, stryker conf, ts lint, tsconfig, mocha, sinon etc.

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,13 +3,50 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
+    this.appConfig = {};
   }
 
-  method1() {
-    this.log('method 1 of app just ran');
+  prompting() {
+    return this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname // Default to current folder name
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your project',
+      default: ''
+    }]).then((answers) => {
+      this.appConfig.appname = answers.name;
+      this.appConfig.description = answers.description;
+    });
   }
 
-  method2() {
-    this.log('method 2 of app just ran');
+  writing() {
+    var context = this.appConfig;
+
+    this.fs.copy(this.sourceRoot() + '/.vscode', context.appname + '/.vscode');
+    this.fs.copy(this.sourceRoot() + '/test', context.appname + '/test');
+    this.fs.copy(this.sourceRoot() + '/.gitignore', context.appname + '/.gitignore');
+    this.fs.copy(this.sourceRoot() + '/stryker.conf.js', context.appname + '/stryker.conf.js');
+    this.fs.copy(this.sourceRoot() + '/tsconfig.json', context.appname + '/tsconfig.json');
+    this.fs.copy(this.sourceRoot() + '/tslint.json', context.appname + '/tslint.json');
+
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath(context.appname + '/README.md'),
+      context
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath(context.appname + '/package.json'),
+      context
+    );
+
+    this.fs.copy(this.sourceRoot() + '/src/index.ts', context.appname + '/src/index.ts', context);
+
+    this.appConfig.installDependencies = true;
   }
 };
